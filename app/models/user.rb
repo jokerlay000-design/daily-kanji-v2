6892653b15e847f2210e20e343c507f3c75971f5
jokerlay000-design <jokerlay000@gmail.com>
2026-07-07 23:26:21 +0900

    Userモデルのhas_manyを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true      
-  has_many :post, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end

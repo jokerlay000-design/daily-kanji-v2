18889eb5e32b358799fa880c53c6a72ad577407c
jokerlay000-design <jokerlay000@gmail.com>
2026-07-07 23:11:23 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "kanjis#index"
   resources :kanjis, only: [:index, :show] do
     resources :posts, only: [:new, :create]
-
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

c48b752dabbaf5322524b331b1995322567fe9bd
jokerlay000-design <jokerlay000@gmail.com>
2026-07-09 22:30:30 +0900

    Commentモデルの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :kanji
   has_one_attached :image
+  has_many :comments, dependent: :destroy
   validates :caption, length: { maximum: 500 }
 end

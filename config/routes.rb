52cee23ddfcb81eeb021dda0d73d00e87efe5f8c
jokerlay000-design <jokerlay000@gmail.com>
2026-07-09 22:33:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
   devise_for :users
   root to: "kanjis#index"
+
   resources :kanjis, only: [:index, :show] do
-    resources :posts, only: [:new, :create]
+    resources :posts, only: [:new, :create] do
+      resources :comments, only: [:create, :destroy]
+    end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
